459fd4816c733897244f3ddd320eadc059dc3316
minh0518 <rubics@naver.com>
2023-02-23 20:01:24 +0900

    feat : 유효성 검사 구현

--- 민호/1-Form Validator/script.js
+++ 민호/1-Form Validator/script.js
@@ -11,6 +11,8 @@ const blankStatus = {
   password2: false,
 };
 
+// blankStatus가 true인 값에 한해서만 진행하며
+// 최종적으로 이게 true면 완료인 것이다
 const validateStatus = {
   username: false,
   email: false,
@@ -20,21 +22,6 @@ const validateStatus = {
 
 // Show input error message
 function showError(input, message) {
-  switch (input) {
-    case 'username':
-      input = username;
-      break;
-    case 'email':
-      input = email;
-      break;
-    case 'password':
-      input = password;
-      break;
-    case 'password2':
-      input = password2;
-      break;
-  }
-
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
@@ -43,27 +30,50 @@ function showError(input, message) {
 
 // Show success outline
 function showSuccess(input) {
-  switch (input) {
-    case 'username':
-      input = username;
-      break;
-    case 'email':
-      input = email;
-      break;
-    case 'password':
-      input = password;
-      break;
-    case 'password2':
-      input = password2;
-      break;
-  }
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${input.id} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${input.id} must be less than ${max} characters`);
+  } else {
+    validateStatus[input.id] = true;
+    showSuccess(input);
+  }
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regex.test(input.value.trim())) {
+    validateStatus[input.id] = true;
+    showSuccess(input);
+  } else {
+    validateStatus[input.id] = true;
+    showError(input, 'Email is not valid');
+  }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  console.log(input1.value, input2.value);
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  } else {
+    validateStatus[input2.id] = true;
+    showSuccess(input2);
+  }
+}
+
 const checkRequired = (inputs) => {
   inputs.forEach((i, index) => {
-    if (i.value.length !== 0) {
+    // 띄워쓰기만 있는 경우도 방지해야 하므로 .length는 적절하지 않음
+    if (i.value.trim() !== '') {
       blankStatus[i.id] = true;
     }
   });
@@ -73,15 +83,58 @@ const showBlankError = () => {
   for (let i in blankStatus) {
     let inputName = i;
     if (blankStatus[inputName] === false) {
+      switch (inputName) {
+        case 'username':
+          inputName = username;
+          break;
+        case 'email':
+          inputName = email;
+          break;
+        case 'password':
+          inputName = password;
+          break;
+        case 'password2':
+          inputName = password2;
+          break;
+      }
       showError(inputName, '공백입니다');
       continue;
     }
   }
 };
 
+const checkValidate = () => {
+  for (let i in blankStatus) {
+    let inputName = i;
+    if (blankStatus[inputName] === false) continue;
+
+    switch (inputName) {
+      case 'username':
+        checkLength(username, 3, 15);
+        break;
+      case 'email':
+        checkEmail(email);
+        break;
+      case 'password':
+        checkLength(password, 6, 25);
+        break;
+      case 'password2':
+        checkPasswordsMatch(password, password2);
+        break;
+    }
+  }
+};
+
+const checkAll = () => {
+  checkRequired([username, email, password, password2]);
+  showBlankError();
+
+  // balnkError와는 달리 유효성 검사와 에러 or 성공 이벤트를 동시에 진행합니다
+  checkValidate();
+};
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
-  showBlankError();
+  if (checkAll()) {
+  }
 });
