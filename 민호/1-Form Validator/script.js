7e8338e3e5a6939d4953aa82d7892054b58d2f37
minh0518 <rubics@naver.com>
2023-02-23 20:13:03 +0900

    feat : 최종 확인 후 alert로 제출 이벤트 발생

--- 민호/1-Form Validator/script.js
+++ 민호/1-Form Validator/script.js
@@ -19,7 +19,7 @@ function showSuccess(input) {
 }
 
 // Check input length
-function checkLength(input, min, max,validateStatus) {
+function checkLength(input, min, max, validateStatus) {
   if (input.value.length < min) {
     showError(input, `${input.id} must be at least ${min} characters`);
   } else if (input.value.length > max) {
@@ -31,7 +31,7 @@ function checkLength(input, min, max,validateStatus) {
 }
 
 // Check email is valid
-function checkEmail(input,validateStatus) {
+function checkEmail(input, validateStatus) {
   const regex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (regex.test(input.value.trim())) {
@@ -44,7 +44,7 @@ function checkEmail(input,validateStatus) {
 }
 
 // Check passwords match
-function checkPasswordsMatch(input1, input2,validateStatus) {
+function checkPasswordsMatch(input1, input2, validateStatus) {
   console.log(input1.value, input2.value);
   if (input1.value !== input2.value) {
     showError(input2, 'Passwords do not match');
@@ -54,7 +54,7 @@ function checkPasswordsMatch(input1, input2,validateStatus) {
   }
 }
 
-const checkRequired = (inputs,blankStatus) => {
+const checkRequired = (inputs, blankStatus) => {
   inputs.forEach((i, index) => {
     // 띄워쓰기만 있는 경우도 방지해야 하므로 .length는 적절하지 않음
     if (i.value.trim() !== '') {
@@ -87,39 +87,36 @@ const showBlankError = (blankStatus) => {
   }
 };
 
-const checkValidate = (blankStatus,validateStatus) => {
-  
+const checkValidate = (blankStatus, validateStatus) => {
   for (let i in blankStatus) {
     let inputName = i;
     if (blankStatus[inputName] === false) continue;
 
     switch (inputName) {
       case 'username':
-        checkLength(username, 3, 15,validateStatus);
+        checkLength(username, 3, 15, validateStatus);
         break;
       case 'email':
-        checkEmail(email,validateStatus);
+        checkEmail(email, validateStatus);
         break;
       case 'password':
-        checkLength(password, 6, 25,validateStatus);
+        checkLength(password, 6, 25, validateStatus);
         break;
       case 'password2':
-        checkPasswordsMatch(password, password2,validateStatus);
+        checkPasswordsMatch(password, password2, validateStatus);
         break;
     }
   }
 };
 
-// 다시 submit했을 경우 기존의 상태값을 죄다 false로 다시 초기화 해야 함
 const checkAll = () => {
-
   const blankStatus = {
     username: false,
     email: false,
     password: false,
     password2: false,
   };
-  
+
   // blankStatus가 true인 값에 한해서만 진행하며
   // 최종적으로 이게 true면 완료인 것이다
   const validateStatus = {
@@ -129,16 +126,26 @@ const checkAll = () => {
     password2: false,
   };
 
-
-  checkRequired([username, email, password, password2],blankStatus);
+  checkRequired([username, email, password, password2], blankStatus);
   showBlankError(blankStatus);
 
   // balnkError와는 달리 유효성 검사와 에러 or 성공 이벤트를 동시에 진행합니다
-  checkValidate(blankStatus,validateStatus);
+  checkValidate(blankStatus, validateStatus);
+
+  let resultFalg = true;
+  for (let i in validateStatus) {
+    if (!validateStatus[i]) {
+      resultFalg = false;
+      break;
+    }
+  }
+
+  return resultFalg;
 };
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if (checkAll()) {
+    alert('제출이 완료되었습니다!');
   }
 });
