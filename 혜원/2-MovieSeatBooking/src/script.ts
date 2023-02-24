21c9b2e1261a694bc21510a913201395c1cdd2be
leehyewon0531 <ifpffk@naver.com>
2023-02-24 17:00:42 +0900

    feat: 선택한 좌석 수 및 가격 변화 기능

--- 혜원/2-MovieSeatBooking/src/script.ts
+++ 혜원/2-MovieSeatBooking/src/script.ts
@@ -1,14 +1,26 @@
-const movie = document.getElementById('movie');
+const movie = <HTMLSelectElement>document.getElementById('movie');
 const seats = document.querySelectorAll('.container .seat');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+let numOfSeats = 0;
 
 function clickSeat (e: Event) {
   const seat = e.target as HTMLDivElement;
   if(seat.className == 'seat') {
     seat.className = 'seat selected';
+    numOfSeats++;
+    count!.innerHTML = `${numOfSeats}`;
+    const price = numOfSeats * Number(movie.value);
+    total!.innerHTML = `${price}`;
     return;
   }
   if(seat.className == 'seat selected') {
     seat.className = 'seat';
+    numOfSeats--;
+    count!.innerHTML = `${numOfSeats}`;
+    const price = numOfSeats * Number(movie.value);
+    total!.innerHTML = `${price}`;
     return;
   }
 }
