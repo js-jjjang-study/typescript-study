f0e4e7f71c3fd0581f21cb55a8adefa00e72d617
minh0518 <rubics@naver.com>
2023-02-26 16:46:39 +0900

    feat : TS로 리팩토링 완료 docs : 기능구현 목록 수정 Chore : tsconfig 의 outdir 수정

--- 민호/1-Form Validator/script.ts
+++ 민호/1-Form Validator/script.ts
@@ -4,67 +4,125 @@ const email = document.getElementById('email') as HTMLElement;
 const password = document.getElementById('password') as HTMLElement;
 const password2 = document.getElementById('password2') as HTMLElement;
 
+// true : 정상 input
+// false : 비정상 input이므로 에러토글 발생
+
 type status = {
-  username: boolean;
-  email: boolean;
-  password: boolean;
-  password2: boolean;
+  [key: string]: boolean; //index signature
 };
 
-function showError(input: HTMLInputElement, message: string) {
-  const formControl: HTMLFormElement = input.parentElement as HTMLFormElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small') as HTMLElement;
-  small.innerText = message;
-}
-
-// Show success outline
-function showSuccess(input: HTMLInputElement) {
-  const formControl: HTMLFormElement = input.parentElement as HTMLFormElement;
-  formControl.className = 'form-control success';
-}
-
-const checkRequired = (inputs: HTMLElement[], blankStatus: status) => {
-  inputs.forEach((i, index) => {
-    // 띄워쓰기만 있는 경우도 방지해야 하므로 .length는 적절하지 않음
-    if ((i as HTMLInputElement).value.trim() !== '') {
-      blankStatus[i.id] = true;
-    }
-  });
-};
-
-const showBlankError = (blankStatus: status) => {
-  for (let i in blankStatus) {
-    let inputName: string = i;
-    let errorTarget: HTMLElement | undefined;
-    if (blankStatus[inputName] === false) {
-      switch (inputName) {
-        case 'username':
-          errorTarget = username;
-          break;
-        case 'email':
-          errorTarget = email;
-          break;
-        case 'password':
-          errorTarget = password;
-          break;
-        case 'password2':
-          errorTarget = password2;
-          break;
-      }
-      showError(errorTarget as HTMLInputElement, '공백입니다');
-      continue;
-    }
-  }
-};
-
-// Check input length
-function checkLength(
+type showErrorFunc = (input: HTMLInputElement, message: string) => void;
+type showSuccessFunc = (input: HTMLInputElement) => void;
+type checkRequiredFunc = (blankStatus: status, validateStatus: status) => void;
+type checkLengthFunc = (
   input: HTMLInputElement,
   min: number,
   max: number,
   validateStatus: status,
-) {
+) => void;
+type checkEmailFunc = (input: HTMLInputElement, validateStatus: status) => void;
+type checkPasswordsMatchFunc = (
+  input1: HTMLInputElement,
+  input2: HTMLInputElement,
+  validateStatus: status,
+) => void;
+type checkValidateFunc = (blankStatus: status, validateStatus: status) => void;
+type checkAllFunc = () => boolean;
+
+type inputTypes = 'username' | 'email' | 'password' | 'password2';
+type checkTypes = 'blank' | 'validate';
+
+const switchFunc = (
+  blankStatus: status,
+  validateStatus: status,
+  checkType: checkTypes,
+) => {
+  if (checkType === 'blank') {
+    [username, email, password, password2].forEach((i, index) => {
+      // 띄워쓰기만 있는 경우도 방지해야 하므로 .length는 적절하지 않음
+      if ((i as HTMLInputElement).value.trim() !== '') {
+        blankStatus[i.id] = true;
+      }
+    });
+
+    for (let i in blankStatus) {
+      let inputName: inputTypes = i as inputTypes; // ?
+      let errorTarget: HTMLElement | undefined;
+      if (blankStatus[inputName] === false) {
+        switch (inputName) {
+          case 'username':
+            errorTarget = username;
+            break;
+          case 'email':
+            errorTarget = email;
+            break;
+          case 'password':
+            errorTarget = password;
+            break;
+          case 'password2':
+            errorTarget = password2;
+            break;
+        }
+        showError(errorTarget as HTMLInputElement, '공백입니다');
+        continue;
+      }
+    }
+  }
+
+  if (checkType === 'validate') {
+    for (let i in blankStatus) {
+      let inputName: inputTypes = i as inputTypes; // ?
+
+      if (blankStatus[inputName] === false) continue;
+      switch (inputName) {
+        case 'username':
+          checkLength(username as HTMLInputElement, 3, 15, validateStatus);
+          break;
+        case 'email':
+          checkEmail(email as HTMLInputElement, validateStatus);
+          break;
+        case 'password':
+          checkLength(password as HTMLInputElement, 6, 25, validateStatus);
+          break;
+        case 'password2':
+          checkPasswordsMatch(
+            password as HTMLInputElement,
+            password2 as HTMLInputElement,
+            validateStatus,
+          );
+          break;
+      }
+    }
+  }
+};
+
+const showError: showErrorFunc = (input: HTMLInputElement, message: string) => {
+  const formControl: HTMLFormElement = input.parentElement as HTMLFormElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small') as HTMLElement;
+  small.innerText = message;
+};
+
+// Show success outline
+const showSuccess: showSuccessFunc = (input: HTMLInputElement) => {
+  const formControl: HTMLFormElement = input.parentElement as HTMLFormElement;
+  formControl.className = 'form-control success';
+};
+
+const checkRequired: checkRequiredFunc = (
+  blankStatus: status,
+  validateStatus: status,
+) => {
+  switchFunc(blankStatus, validateStatus, 'blank');
+};
+
+// Check input length
+let checkLength: checkLengthFunc = (
+  input: HTMLInputElement,
+  min: number,
+  max: number,
+  validateStatus: status,
+) => {
   if (input.value.length < min) {
     showError(input, `${input.id} must be at least ${min} characters`);
   } else if (input.value.length > max) {
@@ -73,61 +131,45 @@ function checkLength(
     validateStatus[input.id] = true;
     showSuccess(input);
   }
-}
+};
 // Check email is valid
-function checkEmail(input: HTMLInputElement, validateStatus: status) {
+let checkEmail: checkEmailFunc = (
+  input: HTMLInputElement,
+  validateStatus: status,
+) => {
   const regex: RegExp =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (regex.test(input.value.trim())) {
     validateStatus[input.id] = true;
     showSuccess(input);
   } else {
-    validateStatus[input.id] = true;
+    validateStatus[input.id] = false; //
     showError(input, 'Email is not valid');
   }
-}
+};
 
 // Check passwords match
-function checkPasswordsMatch(
+let checkPasswordsMatch: checkPasswordsMatchFunc = (
   input1: HTMLInputElement,
   input2: HTMLInputElement,
   validateStatus: status,
-) {
+) => {
   if (input1.value !== input2.value) {
     showError(input2, 'Passwords do not match');
   } else {
     validateStatus[input2.id] = true;
     showSuccess(input2);
   }
-}
-
-const checkValidate = (blankStatus: status, validateStatus: status) => {
-  for (let i in blankStatus) {
-    let inputName: string = i;
-    if (blankStatus[inputName] === false) continue;
-
-    switch (inputName) {
-      case 'username':
-        checkLength(username as HTMLInputElement, 3, 15, validateStatus);
-        break;
-      case 'email':
-        checkEmail(email as HTMLInputElement, validateStatus);
-        break;
-      case 'password':
-        checkLength(password as HTMLInputElement, 6, 25, validateStatus);
-        break;
-      case 'password2':
-        checkPasswordsMatch(
-          password as HTMLInputElement,
-          password2 as HTMLInputElement,
-          validateStatus,
-        );
-        break;
-    }
-  }
 };
 
-const checkAll = () => {
+const checkValidate: checkValidateFunc = (
+  blankStatus: status,
+  validateStatus: status,
+) => {
+  switchFunc(blankStatus, validateStatus, 'validate');
+};
+
+const checkAll: checkAllFunc = () => {
   const blankStatus: status = {
     username: false,
     email: false,
@@ -144,10 +186,7 @@ const checkAll = () => {
     password2: false,
   };
 
-  checkRequired([username, email, password, password2], blankStatus);
-  showBlankError(blankStatus);
-
-  // balnkError와는 달리 유효성 검사와 에러 or 성공 이벤트를 동시에 진행합니다
+  checkRequired(blankStatus, validateStatus);
   checkValidate(blankStatus, validateStatus);
 
   let resultFlag: boolean = true;
