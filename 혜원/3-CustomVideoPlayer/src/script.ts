17660182c3cae51aa9fbe48e7da6540e44a6f6dc
leehyewon0531 <ifpffk@naver.com>
2023-03-02 22:57:35 +0900

    feat: add play & pause btn logic

---
+++ 혜원/3-CustomVideoPlayer/src/script.ts
@@ -0,0 +1,39 @@
+const video = document.getElementById('video') as HTMLVideoElement;
+const playBtn = document.getElementById('play') as HTMLButtonElement;
+const stopBtn = document.getElementById('stop') as HTMLButtonElement;
+const i = playBtn.firstChild?.nextSibling as HTMLElement;
+const classList = i.classList;
+
+function playOrPause() {
+
+  if(classList.contains('fa-play')) {
+    videoPlay();
+    return;
+  }
+
+  if(classList.contains('fa-pause')) {
+    videoPause();
+    return;
+  }
+
+}
+
+function videoPlay() {
+  classList.replace('fa-play', 'fa-pause');
+  video.play();
+  return;
+}
+
+function videoPause() {
+  classList.replace('fa-pause', 'fa-play');
+  video.pause();
+  return;
+}
+
+function videoStop() {
+
+}
+
+video.addEventListener('click', videoPlay);
+playBtn.addEventListener('click', playOrPause);
+stopBtn.addEventListener('click', videoStop);
