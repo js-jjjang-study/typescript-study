66e9bb982cab232507af83a8f881711f8ead24d2
minh0518 <rubics@naver.com>
2023-03-06 17:16:43 +0900

    refactor : 코드 개선 및 리팩토링 Comment : 주석 추가

--- 민호/3-Custom Video Player/src/script.ts
+++ 민호/3-Custom Video Player/src/script.ts
@@ -4,23 +4,26 @@ const stopButton = document.getElementById('stop') as HTMLButtonElement;
 const progressBar = document.getElementById('progress') as HTMLInputElement;
 const timestamp = document.getElementById('timestamp') as HTMLSpanElement;
 
+// 상태 수정 함수 , 상태 리턴 함수
 type setStateFunc<T> = (state: T) => void;
 type stateFunc<T> = () => T;
+
+// 상태가 가질 수 있는 값
+type TF=true|false
+
+// 상태 인터페이스
+interface playStateObj {
+  play: TF; // false : 일시정지 or 정지 ,  true : 재생
+}
 
 type playOrStopFunc = () => void;
 type swapIconFunc = () => void;
 type changeProgressBarFunc = () => void;
+type setVideoByProgressbarFunc = () => void;
 type changeTimeFunc = () => void;
 type stopVideoFunc = () => void;
-type setVideoByProgressbarFunc = () => void;
 
-interface playStateObj {
-  play: any;
-  // false >> 일시정지 or 정지
-  // true >> 재생
-}
-
-// 상태 변경 함수
+// [상태,상태 변경 함수]를 리턴하는 changeState함수
 const changeState = <T extends playStateObj>(status: T): [stateFunc<T>, setStateFunc<T>] => {
 
   let initialState = status;
@@ -34,10 +37,15 @@ const changeState = <T extends playStateObj>(status: T): [stateFunc<T>, setState
   return [state, setState];
 };
 
+
+// 초기 상태 선언
 const initialState: playStateObj = { play: false };
 const [playState, setPlayState] = changeState(initialState);
 
+
+// 일시 정지 , 재생을 다루는 함수
 const playOrStop: playOrStopFunc = () => {
+
   // 재생중일 때
   if (playState().play) {
     video.pause();
@@ -55,12 +63,14 @@ const playOrStop: playOrStopFunc = () => {
   }
 };
 
+// 동영상 진행에 따른 프로그래스바 이동 함수
 const changeProgressBar: changeProgressBarFunc = () => {
   const current: string = String((video.currentTime / video.duration) * 100);
 
   progressBar.value = current;
 };
 
+// 동영상 진행에 따른 하단부의 시간을 업데이트 하는 함수
 const changeTime: changeTimeFunc = () => {
   let min: number = Math.floor(video.currentTime / 60);
   let second: number = Math.floor(video.currentTime % 60);
@@ -68,22 +78,27 @@ const changeTime: changeTimeFunc = () => {
   timestamp.innerHTML = `${min}:${second}`;
 };
 
+// 프로그래스바 변경시 , 동영상 위치 조정 함수
 const setVideoByProgressbar: setVideoByProgressbarFunc = () => {
   video.currentTime = (Number(progressBar.value) * video.duration) / 100;
 };
 
+// 동영상 정지 함수
 const stopVideo: stopVideoFunc = () => {
-  setPlayState({ play: true }); // 일시정지일때(false) playOrStop()을 호출하면
-  // 정지를 하자마자 다시 동영상이 재생되는 로직이 실행되므로
-  // 정지할때만 이렇게 임의로 상태값을 강제로 true로 변경
+  if(playState().play===false){
+    setPlayState({ play: true }); 
+  }
   video.currentTime = 0;
   playOrStop();
 
   // video.pause();
+	// video.currentTime = 0;
   // setPlayState({ play: false });
   // playButton.innerHTML = '<i class="fa fa-play fa-2x"></i>';
 };
 
+
+// 이벤트 할당
 video.addEventListener('click', playOrStop);
 video.addEventListener('timeupdate', changeTime);
 video.addEventListener('timeupdate', changeProgressBar);
