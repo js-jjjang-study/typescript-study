45451232bb4a970faf42021d500505e27730a234
soohyun <bmy1320@gmail.com>
2023-03-07 17:07:48 +0900

    feat: 프로젝트 기능 구현 완료

--- 수현/3Week/src/script.ts
+++ 수현/3Week/src/script.ts
@@ -1,22 +1,50 @@
-/*
-- Display custom video player styled with CSS
-- Play/pause
-- Stop
-- Video progress bar
-- Set progress bar time
-- Display time in mins and seconds
-*/
-{
-  const video = document.getElementById("video") as HTMLVideoElement;
-  const play = document.getElementById("play");
-  const stop = document.getElementById("stop");
-  const progress = document.getElementById("progress");
-  const timestamp = document.getElementById("timestamp");
+type changeValueFunc = void;
 
-  video?.addEventListener("click");
-  video?.addEventListener("play");
-  video?.addEventListener("pasue");
-  video?.addEventListener("setTime");
+const video = document.getElementById("video") as HTMLVideoElement;
+const play = document.getElementById("play") as HTMLButtonElement;
+const Stop = document.getElementById("stop") as HTMLButtonElement;
+const progress = document.getElementById("progress") as HTMLInputElement;
+const timestamp = document.getElementById("timestamp") as HTMLSpanElement;
 
-  // Play/pause
-}
+// - Play/pause
+const videoPlayToggle = (): void => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  updateIcon();
+};
+
+const updateIcon = (): void => {
+  play.innerHTML = video.paused
+    ? '<i class="fa fa-play fa-2x"></i>'
+    : '<i class="fa fa-pause fa-2x"></i>';
+};
+
+// - Stop
+const stopVideo = (): void => {
+  video.currentTime = 0;
+  video.pause();
+};
+
+const updateProgressBar = (): void => {
+  progress.value = String((video.currentTime / video.duration) * 100);
+  const minutes: string = `0${~~(video.currentTime / 60)}`.slice(-2);
+  const seconds: string = `0${~~(video.currentTime % 60)}`.slice(-2);
+
+  timestamp!.innerHTML = `${minutes}:${seconds}`;
+};
+
+const setTime = (): void => {
+  video.currentTime = (+progress.value * video.duration) / 100;
+};
+
+video.addEventListener("click", videoPlayToggle);
+video.addEventListener("timeupdate", updateProgressBar);
+
+play.addEventListener("click", videoPlayToggle);
+
+Stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setTime);
