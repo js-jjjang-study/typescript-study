f08a5dacfea978f834f9366e920f009b2fd8c890
soohyun <bmy1320@gmail.com>
2023-03-14 17:21:40 +0900

    feat: 타입 추가

--- 수현/4Week/src/script.ts
+++ 수현/4Week/src/script.ts
@@ -9,46 +9,54 @@
 
 const fromCurrencyList = document.getElementById(
   "currency-one"
-) as HTMLSelectElement;
+)! as HTMLSelectElement;
 const originalAmount = document.getElementById(
   "amount-one"
-) as HTMLInputElement;
+)! as HTMLInputElement;
 
 const toCurrencyList = document.getElementById(
   "currency-two"
-) as HTMLSelectElement;
+)! as HTMLSelectElement;
 const convertedAmount = document.getElementById(
   "amount-two"
-) as HTMLInputElement;
-const rate = document.getElementById("rate") as HTMLElement;
-const swap = document.getElementById("swap") as HTMLElement;
+)! as HTMLInputElement;
+const rate = document.getElementById("rate")!;
+const swap = document.getElementById("swap")!;
 
-const requestCurrencyAPI = async (fromCurrency: string, toCurrency: string) => {
-  let exchangeInfo = "";
-  let convertAmount = "";
-  const response = await fetch(
-    ` https://v6.exchangerate-api.com/v6/22398a0e5b55547fdb4f7bf3/latest/${fromCurrency}`
-  );
-  if (!response) {
-    console.log("환율 정보를 로드하는데 실패하였습니다.");
+const requestCurrencyAPI = async (
+  fromCurrency: string,
+  toCurrency: string
+): Promise<[string, string]> => {
+  try {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/22398a0e5b55547fdb4f7bf3/latest/${fromCurrency}`
+    );
+    if (!response.ok) {
+      throw new Error("환율 정보를 로드하는데 실패하였습니다.");
+    }
+
+    const data = await response.json();
+    const toCurrencyValue = data.conversion_rates[toCurrency];
+    const exchangeInfo = `1 ${fromCurrency} = ${toCurrencyValue} ${toCurrency}`;
+    const convertAmount = (+originalAmount.value * toCurrencyValue).toFixed(2);
+    return [exchangeInfo, convertAmount];
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-
-  const data = await response.json();
-  const toCurrencyValue = data.conversion_rates[toCurrency];
-
-  exchangeInfo = `1 ${fromCurrency} = ${toCurrencyValue} ${toCurrency}`;
-  convertAmount = (+originalAmount.value * toCurrencyValue).toFixed(2);
-
-  return [exchangeInfo, convertAmount];
 };
 
 const exchange = async () => {
-  const [rateInfo, convertResult] = await requestCurrencyAPI(
-    fromCurrencyList.value,
-    toCurrencyList.value
-  );
-  convertedAmount.value = convertResult;
-  rate.innerText = rateInfo;
+  try {
+    const [rateInfo, convertResult] = await requestCurrencyAPI(
+      fromCurrencyList.value,
+      toCurrencyList.value
+    );
+    convertedAmount.value = convertResult;
+    rate.innerText = rateInfo;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const swapHandler = () => {
