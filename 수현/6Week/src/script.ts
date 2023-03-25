6c939aefa64264a6c00b1bfd180f1da55d0e3b04
soohyun <bmy1320@gmail.com>
2023-03-26 03:54:16 +0900

    feat: 6주차 과제 구현

---
+++ 수현/6Week/src/script.ts
@@ -0,0 +1,44 @@
+/*
+Create and style landing page
+Style side nav and modal
+Add functionality to make menu open/close on button click
+Add functionality to make modal open/close on button click
+*/
+{
+  const navbar = document.getElementById("navbar") as HTMLElement;
+  const toggleBtn = document.getElementById("toggle") as HTMLButtonElement;
+  const modal = document.getElementById("modal") as HTMLButtonElement;
+  const openBtn = document.getElementById("open") as HTMLButtonElement;
+  const closeBtn = document.getElementById("close") as HTMLButtonElement;
+  const BODY = document.body;
+
+  // Navbar
+  const closeNavBar = (e: MouseEvent): void => {
+    const isNavbar = e.target === navbar || navbar.contains(e.target as Node);
+    const isToggle =
+      e.target === toggleBtn || toggleBtn.contains(e.target as Node);
+
+    if (BODY.classList.contains("show-nav") && !isNavbar && !isToggle) {
+      toggleBtn.click();
+    }
+  };
+
+  toggleBtn.addEventListener("click", () => {
+    BODY.classList.toggle("show-nav");
+    if (BODY.classList.contains("show-nav")) {
+      BODY.addEventListener("click", closeNavBar);
+    } else {
+      BODY.removeEventListener("click", closeNavBar);
+    }
+  });
+
+  // Modal
+  const showModal = (): void => modal.classList.add("show-modal");
+  const hideModal = (): void => modal.classList.remove("show-modal");
+
+  openBtn.addEventListener("click", showModal);
+  closeBtn.addEventListener("click", hideModal);
+  window.addEventListener("click", (e) =>
+    e.target === modal ? hideModal() : false
+  );
+}
