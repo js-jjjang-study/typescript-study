1dcb609863106f4e1e8bef48d475e742b4439fa7
leehyewon0531 <ifpffk@naver.com>
2023-03-27 21:18:44 +0900

    feat: check form validation

--- 혜원/6-ModalMenuSlider/src/script.ts
+++ 혜원/6-ModalMenuSlider/src/script.ts
@@ -5,6 +5,14 @@ const modal = document.getElementById('modal');
 const openBtn = document.getElementById('open');
 const closeBtn = document.getElementById('close');
 
+const nameField = document.getElementById('name') as HTMLInputElement;
+const emailField = document.getElementById('email') as HTMLInputElement;
+const pwdField = document.getElementById('password') as HTMLInputElement;
+const pwd2Field = document.getElementById('password2') as HTMLInputElement;
+const modalForm = document.querySelector('.modal-form');
+const submitBtn = document.getElementsByClassName('submit-btn');
+const reg = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
 function handleNavBar() {
   if(body!.className == '') {
     body!.className = 'show-nav';
@@ -27,6 +35,33 @@ function hideModal() {
   return;
 }
 
+function checkEmail(email: string): boolean {
+  return reg.test(email);
+}
+
+function checkPwd(pwd: string, pwd2: string): boolean {
+  return (pwd == pwd2);
+}
+
+function isEmpty(str: string): boolean {
+  if(str == '') return true;
+  return false;
+}
+
+function checkFulfill(e: Event) {
+  if(!isEmpty(nameField.value) &&
+    !isEmpty(emailField.value) &&
+    !isEmpty(pwdField.value) &&
+    !isEmpty(pwd2Field.value)) 
+  {
+    if(checkEmail(emailField.value) && checkPwd(pwdField.value, pwd2Field.value)) return true;
+    else e.preventDefault();
+  } else {
+    e.preventDefault();
+  }
+}
+
 toggle?.addEventListener('click', handleNavBar);
 openBtn?.addEventListener('click', showModal);
 closeBtn?.addEventListener('click', hideModal);
+modalForm?.addEventListener('submit', (e) => { checkFulfill(e) });
