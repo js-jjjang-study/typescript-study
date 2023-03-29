5fb2934db76a4730edafb0be2e05f3881e607d45
Collection50 <fkaus65@naver.com>
2023-03-29 13:47:11 +0900

    feat: 슬라이드 메뉴 구현

--- 성민/6 Week/src/README.md
+++ 성민/6 Week/src/README.md
@@ -1,7 +1,9 @@
 # 기능 목록
 
 - [x] 모달창 구현
-- [] 슬라이드 메뉴 구현
+- [x] 슬라이드 메뉴 구현
+
+<br>
 
 # 모달창
 
@@ -22,11 +24,27 @@
 
 1. 메뉴 버튼 클릭
 2. 슬라이더 `nav` 보여지기 (`body`에 `show-nav` 추가)
-3. 재 클릭 시 슬라이더 `nav` 숨기기
+3. 재클릭 시 슬라이더 `nav` 숨기기
 
 <br>
 
-# Issue && Trouble Shooting
+# 배운 점 || 생각해 볼만한 점
 
 - 모달 외부를 클릭했을 때 모달이 닫히도록 하는 기능을 구현하였습니다.
-- 이에 따라 모달이 열려졌을 때 모달 외부를 클릭하는 경우(모달의 상위 노드) 모달이 닫히도록 하는 기능을 추가하였습니다
+- 이에 따라 모달이 열려졌을 때 모달 외부를 클릭하는 경우(모달의 상위 노드) 모달이 닫히도록 하는 기능을 추가하였습니다.
+- 상위 노드를 클릭하는 경우에도 `closeModal` 함수를 호출하여 모달을 닫아주었습니다.
+- 추가적으로 `document.querySelector`를 사용하면 코드의 길이가 길어져 들여쓰기가 발생합니다.
+- 이에 따라 `document.querySelector` 함수를 `selectQuery`에 할당하여 활용하였습니다.
+
+- `showNav` 함수의 반환 타입은 `boolean`을 사용해야 합니다.
+- 하지만 `showNav` 함수의 목적은 어떠한 값을 반환하는 것이 아니므로 아래와 같은 사용법이 더 적절할 수 있습니다.
+
+```ts
+// before
+const showNav = (): boolean => bodyElement.classList.toggle(showNavClass);
+
+// after
+const showNav = (): void => {
+  bodyElement.classList.toggle(showNavClass);
+};
+```
--- 성민/6 Week/src/script.ts
+++ 성민/6 Week/src/script.ts
@@ -4,16 +4,15 @@ const modalContainer = selectQuery('.modal-container') as HTMLElement;
 const openModalButton = selectQuery('.cta-btn') as HTMLButtonElement;
 const closeModalButton = selectQuery('.close-btn') as HTMLButtonElement;
 
-const showModalClass: string = 'show-modal';
+const bodyElement = document.body;
+const navToggleButton = selectQuery('.toggle') as HTMLButtonElement;
 
-const showModal = (): void => {
-  modalContainer.classList.add(showModalClass);
-  modalContainer.style.display = 'flex';
-};
-const closeModal = (): void => {
-  modalContainer.classList.remove(showModalClass);
-  modalContainer.style.display = 'none';
-};
+const showModalClass: string = 'show-modal';
+const showNavClass: string = 'show-nav';
+
+const showModal = (): void => modalContainer.classList.add(showModalClass);
+const closeModal = (): void => modalContainer.classList.remove(showModalClass);
+const showNav = (): boolean => bodyElement.classList.toggle(showNavClass);
 
 window.onclick = ({ target }) => {
   if (target === modalContainer) {
@@ -22,3 +21,4 @@ window.onclick = ({ target }) => {
 };
 openModalButton.onclick = showModal;
 closeModalButton.onclick = closeModal;
+navToggleButton.onclick = showNav;
