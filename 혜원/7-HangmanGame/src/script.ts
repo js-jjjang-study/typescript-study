06df3384aaec2ca02419d808bce6b9873a9fe72c
leehyewon0531 <ifpffk@naver.com>
2023-04-03 18:30:15 +0900

    feat: show notification

--- 혜원/7-HangmanGame/src/script.ts
+++ 혜원/7-HangmanGame/src/script.ts
@@ -2,6 +2,8 @@ const testWord: string = 'typescript';
 
 const figurePart = document.getElementsByClassName('figure-part');
 const word = document.getElementById('word');
+const notification = document.getElementById('notification-container');
+const inputKeys = new Array<string>;
 
 makeWord();
 
@@ -14,3 +16,33 @@ function makeWord() {
     word?.insertAdjacentElement('beforeend', letter);
   }
 }
+
+function checkKey(e: KeyboardEvent) {
+  const key = e.key;
+
+  if(inputKeys.includes(key)) {
+    showNotification();
+    return;
+  }
+
+  if(!inputKeys.includes(key)) {
+    inputKeys.push(key);
+    return;
+  }
+
+  // keys 에 key 가 있나 확인
+  // 있으면 팝업
+  // 없으면 word에 포함되어 있나 확인
+  // 포함되어 있으면 아래 변경
+  // 포함되어 있지 않으면 오른쪽 변경
+  
+}
+
+function showNotification() {
+  notification!.className += ' show';
+  setTimeout(() => {
+    notification!.className = 'notification-container';
+  }, 2000);
+}
+
+window.addEventListener("keydown", (e) => checkKey(e));
